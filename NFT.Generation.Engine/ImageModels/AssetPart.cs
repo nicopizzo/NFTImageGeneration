91b8c40d54b1210a8ff884009249c7fff1c79b45
Nico Pizzo <funkiernails@yahoo.com>
2022-01-23 19:27:23 -0500

    added support for eyes

--- NFT.Generation.Engine/ImageModels/AssetPart.cs
+++ NFT.Generation.Engine/ImageModels/AssetPart.cs
@@ -3,13 +3,14 @@
     public enum AssetPart
     {
         Head = 1,
-        Face = 2,
-        OuterClothing = 3,
-        Accesories = 4,
-        InnerClothing = 5,
-        Outline = 6,
-        SkinColor = 7,
-        Belly = 8,
-        Background = 9
+        Eyes = 2,
+        Face = 3,
+        OuterClothing = 4,
+        Accesories = 5,
+        InnerClothing = 6,
+        Outline = 7,
+        SkinColor = 8,
+        Belly = 9,
+        Background = 10
     }
 }
